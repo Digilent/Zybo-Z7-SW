b25358ba8e558561f4e1997bfaa99282536df1a1
Radu-Vele <raduvele4@gmail.com>
2022-08-03 16:21:41 +0300

    Configured project for Vitis 2022.1

--- src/pcam_hdmi/45_standalone_app.tcl
+++ src/pcam_hdmi/45_standalone_app.tcl
@@ -21,7 +21,7 @@ set sysproj "pcam_hdmi_system"
 
 # Handle dependent variables
 if {$lang == "c"} {
-	set template "Empty Application(C)"
+	set template "Empty Application"
 } elseif {$lang == "c++"} {
 	set template "Empty Application (C++)"
 } else {

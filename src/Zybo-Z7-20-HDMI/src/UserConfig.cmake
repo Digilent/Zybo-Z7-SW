75e7b421447c60c699310ebc9f4279fdda6ccbe1
robert_bocos <robertemilian.bocos@emerson.com>
2025-07-16 14:42:19 +0300

    Upgraded sw project to Vitis v2025.1. HW XSA import from commit https://github.com/Digilent/Zybo-Z7-HW/tree/c6ec84def38e02c9958d19b6eef3d545fe4c8509

--- src/Zybo-Z7-20-HDMI/src/UserConfig.cmake
+++ src/Zybo-Z7-20-HDMI/src/UserConfig.cmake
@@ -27,6 +27,14 @@ set(USER_UNDEFINED_SYMBOLS
 
 set(USER_INCLUDE_DIRECTORIES
 )
+set(USER_COMPILE_SOURCES
+"video_demo.c"
+"display_ctrl.c"
+"dynclk.c"
+"intc.c"
+"timer_ps.c"
+"video_capture.c"
+)
 
 # -----------------------------------------
 
